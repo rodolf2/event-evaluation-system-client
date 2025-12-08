303a7b50c114d5064f88e8cef19d4d09e95e9915
rodolf2 <rodolfojrxgt@gmail.com>
2025-12-08 23:21:10 +0800

    Changed the target url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
-const apiUrl = import.meta.env.VITE_BASE_API_URL;
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,18 +9,18 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: apiUrl,
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
       "/thumbnails": {
-        target: apiUrl,
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
       "/uploads": {
-        target: apiUrl,
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
